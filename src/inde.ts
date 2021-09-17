cca153065c58e5a7a7a6adb6f084d69d407b94e7
Juan Camargo <juan.09.camargo@gmail.com>
2021-09-17 12:10:44 -0300

    finish

--- src/inde.ts
+++ src/inde.ts
@@ -1,5 +1,17 @@
 import express from 'express';
+import { URLController } from './controller/URLController'
+import { MongoConnection } from './database/MongoConnection'
+
+
 const port=5000
 
 const app = express()
+app.use(express.json())
+
+const database = new MongoConnection()
+database.connect()
+
+const urlController = new URLController()
+app.post('/shorten', urlController.shorten)
+app.get('/:hash',urlController.redirect)
 app.listen(port, () => console.log('serve is ruing'))
